5edfd5f329c4e131a21806bcc675182f416e87af
kalet Chavez <kalet.svdev@gmail.com>
2024-08-28 22:59:47 -0600

    :construction: changes and probs about base converter

--- base_converter.cpp
+++ base_converter.cpp
@@ -1,6 +1,50 @@
+#include <string>
+#include <iostream>
+#include <algorithm>
 
+using namespace std;
 
+string convertDecimal( int number ){
+
+    string binaryNumber = "";
+
+    int decrement = number;
+
+    while ( decrement > 0 ){
+        cout << "decrement" << decrement << endl;
+        if ( decrement == 1 ){
+
+            binaryNumber = binaryNumber + to_string(1);
+            decrement--;
+
+        }
+        else if( decrement % 2 == 1 ){
+            
+            decrement = decrement -1;
+            binaryNumber = binaryNumber + to_string(1);
+
+        }
+        else if( decrement % 2 == 0 ){
+          
+            int newNumber = decrement / 2;
+            decrement = newNumber;
+            binaryNumber = binaryNumber + to_string(0);
+
+        }
+    }
+    reverse( binaryNumber.begin(), binaryNumber.end() );
+    return binaryNumber;
+
+}
 
 int main (){
-    
+
+    int number;
+    cout << "Ingrese su numero decimal" << endl;
+    cin >> number;
+
+    string binaryNumber = convertDecimal( number );
+
+    cout << "Su numero binario es: " << binaryNumber << endl;
+
 }
